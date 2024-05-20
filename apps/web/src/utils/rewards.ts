bd7b6b518f4e8dc5c5f1d34e27ad2029849c0420
alfanaldoh <alfanaldoh@gmail.com>
2024-05-20 09:24:05 +0700

    feat: finish

--- apps/web/src/utils/rewards.ts
+++
@@ -1,9 +0,0 @@
-// import { User } from "@/types/user.type";
-
-// export const rewards = (user: User): number => {
-//     if (!user.rewards) {
-//       return 0;
-//     }
-  
-//     return (user.rewards
-//   };
